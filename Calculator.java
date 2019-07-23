3d786fe2f3c7f4db8bbdf6044bdd3d89f836595e
Sindhuja1995 <53013555+Sindhuja1995@users.noreply.github.com>
2019-07-23 13:49:34 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -15,11 +15,11 @@
     public static void main(String[] args) 
     {    
     	JavaExample obj = new JavaExample();
-    	//This will call the first add method
+    	
         System.out.println("Sum of two numbers: "+obj.add(10, 20));
-        //This will call second add method
+       
         System.out.println("Sum of three numbers: "+obj.add(10, 20, 30));
-        //This will call third add method
+      
         System.out.println("Sum of four numbers: "+obj.add(1,  2, 3, 4));
     }
 }
