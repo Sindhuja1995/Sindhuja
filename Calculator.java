87c119cf7bc345d0a70eea7cc8886d08b8c42bdb
Sindhuja1995 <53013555+Sindhuja1995@users.noreply.github.com>
2019-07-23 13:48:40 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,57 +1,27 @@
-	import java.util.Scanner;
-	
-	
-public class Calculator {
+	public class Calculator {
 
-
-	    public static void main(String[] args) {
-
-	    	double num1, num2;
-	        Scanner scanner = new Scanner(System.in);
-	        System.out.print("Enter first number:");
-
-	        /* We are using data type double so that user
-	         * can enter integer as well as floating point
-	         * value
-	         */
-	        num1 = scanner.nextDouble();
-	        System.out.print("Enter second number:");
-	        num2 = scanner.nextDouble();
-
-	        System.out.print("Enter an operator (+, -, *, /): ");
-	        char operator = scanner.next().charAt(0);
-
-	        scanner.close();
-	        double output;
-
-	        switch(operator)
-	        {
-	            case '+':
-	            	output = num1 + num2;
-	                break;
-
-	            case '-':
-	            	output = num1 - num2;
-	                break;
-
-	            case '*':
-	            	output = num1 * num2;
-	                break;
-
-	            case '/':
-	            	output = num1 / num2;
-	                break;
-
-	            /* If user enters any other operator or char apart from
-	             * +, -, * and /, then display an error message to user
-	             * 
-	             */
-	            default:
-	                System.out.printf("You have entered wrong operator");
-	                return;
-	        }
-
-	        System.out.println(num1+" "+operator+" "+num2+": "+output);
-	    }
+    int add(int num1, int num2)
+    {
+        return num1+num2;
+    }
+    int add(int num1, int num2, int num3)
+    {
+        return num1+num2+num3;
+    }
+    int add(int num1, int num2, int num3, int num4)
+    {
+        return num1+num2+num3+num4;
+    }
+    public static void main(String[] args) 
+    {    
+    	JavaExample obj = new JavaExample();
+    	//This will call the first add method
+        System.out.println("Sum of two numbers: "+obj.add(10, 20));
+        //This will call second add method
+        System.out.println("Sum of three numbers: "+obj.add(10, 20, 30));
+        //This will call third add method
+        System.out.println("Sum of four numbers: "+obj.add(1,  2, 3, 4));
+    }
+}
 	}
 
